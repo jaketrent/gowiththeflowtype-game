157881fcb5c36416bafa614d518677926c85e3c4
jaketrent <trent.jake@gmail.com>
2018-04-11 17:22:02 -0600

    refactor: support multiple middleware in route

--- src/routes.js
+++ src/routes.js
@@ -7,11 +7,13 @@ import page from 'page'
 import index from './pages/index'
 import error from './pages/error'
 
-const route = (path: string, handler: (Router, RouteContext) => void) =>
-  page(path, (ctx: RouteContext) => handler(page, ctx))
+const route = (
+  path: string,
+  ...middleware: ((Router, RouteContext) => void)[]
+) => page(path, ...middleware.map(m => m.bind(null, page)))
 
 export const map = () => {
-  route('/', page => page.redirect('/0'))
+  route('/', router => router.redirect('/0'))
   route('/:id', index)
   route('/error/:code', error)
   route('*', error)
